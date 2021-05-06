3a91e3ca845ad6203303afeddf0fe7b34bfc0cbb
Hari <harry.prashantb@gmail.com>
2021-05-06 20:28:22 +0530

    accounts/orders/products: Change page size and return pages count.

--- accounts/pagination.py
+++ accounts/pagination.py
@@ -1,6 +1,11 @@
 from rest_framework.pagination import PageNumberPagination
 
 class ResultSetPagination(PageNumberPagination):
-    page_size = 30
+    page_size = 20
     page_size_query_param = 'page_size'
-    max_page_size = 50
+    max_page_size = 20
+
+    def get_paginated_response(self, data):
+        response = super(ResultSetPagination, self).get_paginated_response(data)
+        response.data['total_pages'] = self.page.paginator.num_pages
+        return response
