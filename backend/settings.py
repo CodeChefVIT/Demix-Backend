ad4d5dcbdd1eeef1ec25a168a0d598744d018c19
Hari <harry.prashantb@gmail.com>
2021-05-09 04:22:01 +0530

    backend: Set email host through environment variables.

--- backend/settings.py
+++ backend/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
 
 # Email Backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.in'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
